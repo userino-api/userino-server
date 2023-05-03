9b70f4720ed743ae650c26b6d3a34da4ba86a3d0
Vlad_Zainchkovskyi <zvsx001@gmail.com>
2023-05-04 01:01:11 +0300

    feat: authorize app by headers

--- api/user/middlewares/appMiddleWare.ts
+++ api/user/middlewares/appMiddleWare.ts
@@ -1,11 +1,22 @@
 import { RequestHandler } from 'express'
-import invariant from 'invariant'
-import appsModel from '@models/appsModel'
+import appsModel, { App } from '@models/appsModel'
+import config from '../../../config/settings'
 
 function createAppMiddleware(): RequestHandler {
   return async (req, res, next) => {
-    const app = await appsModel.getPrimaryApp()
-    invariant(app, 'no primary app found')
+    const app_id = req.headers['app'] as string
+    let app: App | null = null
+    if (app_id) {
+      app = await appsModel.get(app_id)
+    }
+
+    if (!app && config.app.isPrimaryEnabled) {
+      app = await appsModel.getPrimaryApp()
+    }
+
+    if (!app) {
+      return res.sendError(403, 'No app found')
+    }
 
     req.session = {
       ...req.session,
